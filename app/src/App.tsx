9b7889a2ecba49af7c84826af57c0957589771bf
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-12-15 22:31:28 +0100

    h1 title changed

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,7 @@ import SlotMachine from "./components/Slotmachine/Slotmachine";
 function App() {
   return (
     <>
-      <h1>Labo 2: Slots</h1>
+      <h1>Try your luck !</h1>
 
       <SlotMachine slots={3} />
       <SlotMachine slots={5} />
